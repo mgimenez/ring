bd2d6b94c330ac51eff79a3b6120c0f79f6ed197
Matias Gimenez <mgimenez@MacBook-Pro-de-Agustin.local>
2016-02-26 18:48:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('static'));
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('con');
+    socket.on('chat message', function(socket){
+        console.log('ring');
+        io.emit('chat message');
+    });
+});
